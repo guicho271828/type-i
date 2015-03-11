fb81fa8da6a96c7ca49db3531ff995e30729a4e5
Masataro Asai <guicho2.71828@gmail.com>
2015-03-11 15:48:32 +0900

    fix infer-t

--- src/infer-t.lisp
+++ src/infer-t.lisp
@@ -1,20 +1,8 @@
 (in-package :type-i)
 
 
-;;; null
-
-(defvar +null-tests+
-    `((null ?)
-      (typep ? 'null)
-      (eql ? nil)
-      (eql nil ?)
-      (eq ? nil)
-      (eq nil ?)
-      (equal ? nil)
-      (equal nil ?)
-      (equalp ? nil)
-      (equalp nil ?)))
+;;; t
 
 (define-inference-rule true-tests (test)
   (when (eq test t)
-    '((typep ? 't)))) ;; should be quoted, see definition of test-type
+    '((typep ? (quote t))))) ;; should be quoted, see definition of test-type
